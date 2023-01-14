a590804a9e5d093ffde478d02bfa652487f0bf13
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-01-14 19:24:52 +0000

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home {...user}/>
+      <About bio={user.bio} links={user.links}/>
     </div>
   );
 }
